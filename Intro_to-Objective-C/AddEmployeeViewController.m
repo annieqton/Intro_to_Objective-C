cc15390e3ccd069dd933ecd17b4d5614679b3f2f
Annie Ton-Nu <annieqton@gmail.com>
2017-04-19 13:48:29 -0700

    created AddEmployeeViewController.h and .m

---
+++ Intro_to-Objective-C/AddEmployeeViewController.m
@@ -0,0 +1,28 @@
+//
+//  AddEmployeeViewController.m
+//  Intro_to-Objective-C
+//
+//  Created by Annie Ton-Nu on 4/19/17.
+//  Copyright © 2017 Annie Ton-Nu. All rights reserved.
+//
+
+#import "AddEmployeeViewController.h"
+
+@interface AddEmployeeViewController ()
+
+@end
+
+@implementation AddEmployeeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
